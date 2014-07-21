3bb913fed3edf602809cbf1daa678d4efcc29809
Kyle Coberly <kyle.coberly@gmail.com>
2014-07-20 21:40:46 -0700

    Component works with images

--- app/components/image-upload.js
+++ app/components/image-upload.js
@@ -6,6 +6,50 @@ export default Ember.Component.extend({
 			return value;
 		}
 	}.property(),
+	progress: function(key, value){
+		if (arguments.length > 1){
+			if (value === 100){
+				return 0;
+			} else {
+				return value;
+			}
+		}
+	}.property(),
+
+	uploadFile: function(){
+		var self = this;
+		var Base64FileReader = new FileReader();
+
+		Base64FileReader.onload = function(){
+			self.set("errorMessage", "");
+			self.set("imageThumbnail", Base64FileReader.result);
+
+			Ember.$.ajax({
+				url: self.get("uploadEndpoint") + "?fileName=" + self.get("file").name,
+				type: "POST",
+				data: self.get("file"),
+				dataType: "JSON",
+				processData: false,
+				success: function(data) {
+					self.set("errorMessage", "");
+					self.set("fileUrl", data.file_url);
+				},
+				error: function() {
+					self.set("imageThumbnail", "");
+					self.set("errorMessage", "Problem uploading file");
+				}
+			});
+		};
+		Base64FileReader.onprogress = function(data) {
+			if (data.lengthComputable) {                                            
+				var progress = parseInt( ((data.loaded / data.total) * 100), 10 );
+				self.set("progress", progress);
+			}
+		}
+
+		Base64FileReader.readAsDataURL(this.get("file"));
+	}.observes("file").on("change"),	
+
 	dragEnter: function(event) {
 		event.preventDefault();
 	},
@@ -22,43 +66,32 @@ export default Ember.Component.extend({
 		}
 		this.set("file", file);
 	},
-	uploadImage: function(){
-		var self = this;
-		var Reader = new FileReader();
 
-		Reader.onload = function(event){
-			Ember.$.ajax({
-				url: this.get("ImageUploadEndpoint"),
-				type: "POST",
-				data: 	{
-							imageData: Reader.result,
-							imageName: value.name
-						},
-				dataType: "JSON",
-				success: function(data) {
-					self.set("errorMessage", "");
-					self.set("imageUrl", data.image_url);
-				},
-				error: function() {
-					self.set("errorMessage", "Problem uploading file");
-				}
-			});
-		};
-		Reader.readAsDataURL(value);
 
-	}.observes("file").on("change", "init"),
 	didInsertElement: function(){
 		var self = this;
 		this.$(".file-input-field").change(function(){
 			var file = self.$(".file-input-field").prop("files")[0];
 			self.set("file", file);
 		});
+		this.$(".image-outline").click(function(){
+			self.$(".file-input-field").click();
+		});
 	},
 
 	actions: {
 		removeImage: function(){
-			this.set("imageUrl", "");
+			var self = this;
+
+			this.set("file", "");
+			this.set("imageThumbnail", "");
+			this.set("fileUrl", "");
+			this.set("errorMessage", "");
 			this.$(".file-input-field").val("");
+
+			this.$(".image-outline").click(function(){
+				self.$(".file-input-field").click();
+			});
 		}
 	}
 });
